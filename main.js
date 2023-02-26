8f717ca42f0a1eddbd3f87fb005bf53bca7ba1f3
sirucekBM <bmartins@seznam.cz>
2023-02-26 18:53:55 +0100

    initial

---
+++ main.js
@@ -0,0 +1,42 @@
+import { Player } from './player.js';
+import { InputHandler } from './input.js';
+
+
+window.addEventListener('load',function(){
+    const canvas = this.document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+
+    class Game{
+        constructor(width,height){
+            this.width = width;
+            this.height = height;
+            this.groundMargin = 50;
+            this.player = new Player(this);
+            this.input = new InputHandler(this);
+            
+        }
+
+        update(deltaTime){
+            this.player.update(this.input.keys,deltaTime);
+        }
+        draw(context){
+            this.player.draw(context)
+        }
+    }
+
+    const game = new Game(canvas.width,canvas.height);
+    console.log(game);
+    let lastTime = 0;
+
+    function animate(timeStamp){
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        game.update(deltaTime);
+        game.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate(0);
+});

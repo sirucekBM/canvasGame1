2488b94c2f86974ddcbde3d07486353122c06eb9
sirucekBM <bmartins@seznam.cz>
2023-03-12 18:48:35 +0100

    add particle

---
+++ particles.js
@@ -0,0 +1,31 @@
+class Particle{
+    constructor(game) {
+        this.game = game;
+        this.markedForDeletion = false;
+    }
+    update(){
+        this.x -= this.speedX + this.game.speed-2;
+        this.y -= this.speedY;
+        this.size *= 0.95;//speed sizing
+        if(this.size <0.5)this.markedForDeletion = true;
+    }
+}
+
+export class Dust extends Particle{
+    constructor(game, x, y){
+        super(game);
+        this.size = 20;// Math.random()* 10 + 10;
+        this.x = x;
+        this.y = y;
+        this.speedX = Math.random();
+        this.speedY = Math.random();
+        this.color = 'white';
+    };
+    draw(context){
+        context.beginPath();
+        context.arc(this.x, this.y, this.size,0,Math.PI * 2);
+        context.fillStyle = this.color;
+        //context.fill();
+        context.stroke();
+    }
+}
